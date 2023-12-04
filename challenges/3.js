e0eedb76af6692e8582c5947588e3ae6b224881e
Elissa Martinez <10544138+elmartinezc@users.noreply.github.com>
2023-12-04 13:19:44 +0100

    day 3 pt 1

---
+++ challenges/3.js
@@ -0,0 +1,41 @@
+import { InputLoader } from "../inputfiles/InputLoader.js";
+
+const inputData = InputLoader.loadChallengeInputAsArray(3);
+
+const numberRegex = /(\d+)/dg;
+const symbolRegex = /[^\d.]/;
+
+const stringSectionContainsSymbol = (lineString, [lowIndex, highIndex]) => {
+  if (lineString) {
+    for (let i = lowIndex - 1; i < highIndex + 1; i++) {
+      if (lineString[i] && lineString[i].match(symbolRegex)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+console.log(
+  "part 1: ",
+  inputData.reduce((prev, curr, index) => {
+    const previous = inputData[index - 1];
+    const next = inputData[index + 1];
+    const numberMatches = curr.matchAll(numberRegex);
+    let aggregatedNumbers = 0;
+    for (const numberMatch of numberMatches) {
+      const number = parseInt(numberMatch[1]);
+      const [lowIndex, highIndex] = numberMatch.indices[0];
+      const [prevChar, nextChar] = [curr[lowIndex - 1], curr[highIndex]];
+      if (
+        (prevChar && prevChar.match(symbolRegex)) ||
+        (nextChar && nextChar.match(symbolRegex)) ||
+        stringSectionContainsSymbol(previous, numberMatch.indices[0]) ||
+        stringSectionContainsSymbol(next, numberMatch.indices[0])
+      ) {
+        aggregatedNumbers += number;
+      }
+    }
+    return prev + aggregatedNumbers;
+  }, 0),
+);
